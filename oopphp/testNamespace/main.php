eee496466c4af4c1d16b058d045564b9257c41d0
maoufikri <fikrimuhammad38993@gmail.com>
2025-03-15 07:50:39 +0700

    tambahan materi namespace

--- oopphp/testNamespace/main.php
+++ oopphp/testNamespace/main.php
@@ -2,10 +2,23 @@
 
 include "induk.php";
 
-$lorem = new Html\lorem;
+use Html\lorem;
+use Html\kembar;
+use matematika\kembar as kembar2;
+use matematika\kembar as kembar3;
+use matematika\segitiga\segitiga;
+// use matematika\segitiga\kembar as kembar3; //error karena tidak ada kelas kembar di matematika\segitiga.
+
+$lorem = new lorem;
 $lorem->lorem();
 
 Html\paragraph::paragraph(); //static method
 
 $kotak = new matematika\persegiPanjang(70,90);
-echo $kotak->keliling() . "\n";
+echo $kotak->keliling()."\n";
+
+$kembar1 = new kembar;
+$kembar2 = new kembar2;
+$kembar3 = new kembar3;
+
+$segitiga = new segitiga(90,80);
